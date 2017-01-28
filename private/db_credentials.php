d94fbd25e62bc7e2c9b659cf8eb8e19075ea5e11
Siraj Zaneer <szaneer@gmail.com>
2017-01-28 04:19:51 -0500

    Salesperson start

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
   define("DB_SERVER", "localhost");
-  define("DB_USER", "siraj");
-  define("DB_PASS", "qwe@3asd");
-  define("DB_NAME", "globitek");
+  define("DB_USER", "root");
+  define("DB_PASS", "");
+  define("DB_NAME", "globitek2");
 ?>
